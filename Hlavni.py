9fc0a2f7f831a91fefd2232acc11f3968d2bd9bd
Marek Bajer <marekbajer07@sezna.cz>
2023-12-05 16:07:18 +0100

    NumPy
    Předělání kódu z čistého Pythonu na program s pomocí NumPy. Přesněji za pomoci np.dot()

--- Hlavni.py
+++ Hlavni.py
@@ -1,17 +1,36 @@
+#Video https://www.youtube.com/watch?v=tMrbN67U9d4&ab_channel=sentdex   Neural Networks from Scratch - P.3 The Dot Product
 import numpy as np
 import matplotlib
 imputs = [1, 2, 3, 2.5 ]
-
 weights = [[ 0.2, 0.8, -0.5, 1.0],
         [0.5, -0.91, 0.26, -0.5],
         [-0.26, -0.27, 0.17, 0.87 ]]
 biases = [2, 3, 0.5]
 
-layer_outputs = [] # vysledky dane vrstvy
-for neuron_weights, neuron_bias in zip(weights, biases): # zip da dohromady dva lysty [0] = weights[0], biases[0]
-    neuron_output = 0 # výsledek danehoneuronu
-    for n_input, weight in zip(imputs, neuron_weights): # da dohromady imputy a vahu neuronu
-        neuron_output += n_input*weight # vytvoři vysledek neuronu
-    neuron_output += neuron_bias # přičte bias k vysledku neuronu
-    layer_outputs.append(neuron_output)
-print(layer_outputs)
+
+output = np.dot(weights, imputs)+ biases # weights musí být vždy v zavorce první protože určuje počet neuronů - 3 listy 
+print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
