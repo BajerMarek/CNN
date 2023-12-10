7c112ceeae5c1e7a47d4efa13a983eb2e98daeb0
Marek Bajer <marekbajer07@sezna.cz>
2023-12-10 17:15:16 +0100

    Přidá možnost generovat druhou vrstvu jak je potřeba

--- Hlavni.py
+++ Hlavni.py
@@ -2,28 +2,32 @@
 #Videohttps://www.youtube.com/watch?v=TEWy9vZcxW4&ab_channel=sentdex
 import numpy as np
 import matplotlib
-imputs = [[1, 2, 3, 2.5 ],
-          [2.0, 5.0, -1.0, 2.0 ],
-          [-1.5, 2.7, 3.3, -0.8]]
 
-weights = [[ 0.2, 0.8, -0.5, 1.0],
-        [0.5, -0.91, 0.26, -0.5],
-        [-0.26, -0.27, 0.17, 0.87 ]]
+np.random.seed(0)
 
-biases = [2, 3, 0.5]
-
-weights2 = [[0.1, -0.14, 0.5],
-        [-0.5, 0.12, -0.33],
-        [-0.44, 0.73, -0.13]]
-
-biases2 = [-1, 2, -0.5]
-
-layer1_outputs1 = np.dot(imputs, np.array(weights).T)+ biases
-layer1_outputs2 = np.dot(layer1_outputs1, np.array(weights2).T)+ biases2
-print(layer1_outputs2)
+X = [[1, 2, 3, 2.5 ],
+    [2.0, 5.0, -1.0, 2.0 ],
+    [-1.5, 2.7, 3.3, -0.8]]
 
 
 
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)     # Určí počet neuroný a imputu
+        self.biases = np.zeros((1, n_neurons))                              # zaroveň zadani v tomto tvaru nám zaručí že není potřeba použít transpose
+        pass
+    def forward(self, imputs):
+        self.output = np.dot(imputs, self.weights) + self.biases
+
+layer1 = Layer_Dense(4,5)                # zde se zadá počet prvků v imputech (zde je to X), a počet neuronu které chceme vytvořit
+layer2 = Layer_Dense(5,2)
+
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
+
+                                    #print(0.10 * np.random.randn(4, 3)) # vysvětleni v dokumetaci
 
 
 
