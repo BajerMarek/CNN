a3c9527a60613c748baac1cd5f589b53b350bdbd
Marek Bajer <marekbajer07@sezna.cz>
2023-12-16 13:23:25 +0100

    Přidání Funkce Softmax do CNN

--- Hlavni.py
+++ Hlavni.py
@@ -1,15 +1,12 @@
-#https://www.youtube.com/watch?v=gmjzbpSVY1A&ab_channel=sentdex
+#https://www.youtube.com/watch?v=omz_NdFgWyU&t=1328s&ab_channel=sentdex
 import numpy as np
 import matplotlib
 import nnfs
 from nnfs.datasets import spiral_data
-nnfs.init()
 #np.random.seed(0)
 
-X = [[1, 2, 3, 2.5 ],
-    [2.0, 5.0, -1.0, 2.0 ],
-    [-1.5, 2.7, 3.3, -0.8]]
-X, y = spiral_data(100,3)
+nnfs.init()
+
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)     # Určí počet neuroný a imputu
@@ -22,52 +19,28 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(2,5)                # zde se zadá počet prvků v imputech (zde je to X), a počet neuronu které chceme vytvořit
-activation1 = Activation_ReLU()
-
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
-
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs,axis=1, keepdims=True))
+        propabilities = exp_values / np.sum(exp_values,axis=1, keepdims=True )
+        self.output = propabilities
 
 
+X,y = spiral_data(samples=100, classes=3)
 
+dense1 = Layer_Dense(2,3)       # první číslo v () počet dat které vrstva přijímá a počet a druhé je počet neuronů které chce me vytvořit.
+activation1 = Activation_ReLU()        # první imput druhé output
 
+dense2= Layer_Dense(3,3)
+activation2 = Activation_Softmax()
 
+dense1.forward(X)
+activation1.forward(dense1.output)
 
-#print(0.10 * np.random.randn(4, 3)) # vysvětleni v dokumetaci
-"""
-inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
-output = []
-for i in inputs:
-    if i > 0:
-        output.append(i)
-    elif i <= 0:
-        output.append(0)
-print(output)
-"""
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
 
-"""
-# data set pro CNN => nahodný data.
-#https://cs231n.github.io/neural-networks-case-study/
-def spiral_data(points, classes):
-    X = np.zeros((points*classes, 2))
-    y = np.zeros(points*classes, dtype='uint8')
-    for class_number in range(classes):
-        ix = range(points*class_number, points*(class_number+1))
-        r = np.linspace(0.0, 1, points)  # radius
-        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
-        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
-        y[ix] = class_number
-    return X, y
-"""
-
-
-
-
-
-
-
+print(activation2.output[:5])
 
 
 
