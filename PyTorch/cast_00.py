e33e1216369b624ec20dcbdff4910615b29a2ff8
Marek Bajer <marekbajer07@sezna.cz>
2023-12-23 13:40:34 +0100

    Datatypy tensoru

--- PyTorch/cast_00.py
+++ PyTorch/cast_00.py
@@ -2,6 +2,7 @@ import torch
 import pandas as pd
 import numpy as np
 import matplotlib as plt
+"""
 ### typy listů / nezorů
 scalar = torch.tensor(7)
 #print(scalar)
@@ -28,5 +29,24 @@ ones = torch.ones(3, 4)     # vytcoří tenzor plný 1
 #print(torch.arange(start=0, end=1000, step=77))     #začátek, konec, každý posun o kolik
 one_to_ten = torch.arange(start=1, end=11, step=1)      # vytvoří řadu 0d 1 - 10
 ten_zeros = torch.zeros_like(input=one_to_ten)      # přemnění řadu na nuli
-print(ten_zeros)
+#print(ten_zeros)
+
+float_32_tensor = torch.tensor([3.0, 6.0, 9.0],
+                               dtype=None,      # data typ tensoru, defaultně float32
+                               device="cpu",    # kde je tensor uložen
+                               requires_grad=False)     # jestli sledovat růst toot tensoru
+#print(float_32_tensor.dtype)
+
+float_16_tensor =  float_32_tensor.type(torch.float16)
+#print(float_16_tensor)
+
+int_32_tensor = torch.tensor([3, 5, 9],dtype=torch.int32)
+#print(float_16_tensor*int_32_tensor)
+"""
+# získávání dat z tensoru
+some_tensor = torch.rand(3, 4)
+print(some_tensor)
+print(f"Datatype of tensor:{some_tensor.dtype}") 
+print(f"Shape of tensor:{some_tensor.shape}") 
+print(f"Device of tensor:{some_tensor.device}") 
 
