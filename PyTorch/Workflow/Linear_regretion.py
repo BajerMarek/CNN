14c9a64fc53afbcf70a7f859631bf7d8770da1ba
Marek Bajer <marekbajer07@sezna.cz>
2023-12-29 10:44:23 +0100

    Smičky dokumentace + Funkční model

--- PyTorch/Workflow/Linear_regretion.py
+++ PyTorch/Workflow/Linear_regretion.py
@@ -65,30 +65,57 @@ torch.manual_seed(42)
 # vyvoření modelu (subclass of nnmodel)
 model_0 = LinearRegressionModel()
 
-# zijsitíme jaké parametry máme v modelu
-#print(list(model_0.parameters()))
-
-# pojmenování parametrů
-#print(model_0.state_dict())
-
 # první předdpovědí y_test na základě X_test
 # když posíláme data přěs náš model tak forward data spracovává
-
 # predikce
+with torch.inference_mode():
+    y_preds = model_0(X_test)
+plot_prediction(predictions=y_preds)
+#plt.show()
+print(model_0.state_dict())
+# nastavení loss funkce
+loss_fn = nn.L1Loss()
+
+# nastavení optimizeru      SGD -> stochastick gradient desent
+optimizer = torch.optim.SGD(params=model_0.parameters(),    # co se má optimalizovat
+                            lr=0.01)    # nejduležitější hyperparametr který si musíme sami naastavit
+
+# Trainig loop a testing loop
+"""
+0. Proskenování dat
+1. Forward pass (prohnání dat skrz metodu forward()) - také nazívané jako forwad propagation
+    za cílem vytvořit nějákou před předpovědí
+2. Zpočítání loss funkce - (srovnání výsledku modelu s správnýmy výsledky)
+3. Optimazer zero grad  nastavý hodnotu optimazeru na 0 aby se nezpomaloval výpočet (jak? nevím)
+4. Loss backkwards -> pohybuje se od výsledku aby zjistíl spád jednolivých parametrů
+   => (spád -> míra významnosti pro výsledek)  - (Back propagation)
+5. Optimazer step - Využití optimizeru k upravě parametrů tak aby se snížila hodnota 
+    z loss funkce. - (Gradiant desent)
+"""
+# epocha = 1 opakování ciklu
+epochs = 1000
+                                                            # 0. Proskenovat dat
+for epochs in range(epochs):
+    model_0.train() # nastavý všechny parametry které potřebukí sklon aby potřebovaly sklon
+    y_pred = model_0(X_train)                               # 1. Forward pass
+    loss = loss_fn(y_pred,y_train)                          # 2. Loss funkce
+    optimizer.zero_grad()                                   # 3. Optimazer zero grad
+    loss.backward()                                       # 4. Back propagation
+    optimizer.step()
+    # testink loop
+    # vypně všechny funkce které by zpomalovalí testování
+    model_0.eval()
+    with torch.inference_mode():    # vypne hlídání gradiantu   take se občas použiva torch.no_grad
+        test_pred = model_0(X_test)                                 # 1. Forward pass
+        test_loss = loss_fn(test_pred,y_test)                       # 2. Loss funkce   
+    if epochs % 10 ==0:
+        print(f"Epoch: {epochs} | Loss: {loss} | Test loss: {test_loss}")
+        print(model_0.state_dict())                                      # 5. Optimizer step
 
 with torch.inference_mode():
     y_preds = model_0(X_test)
 plot_prediction(predictions=y_preds)
 plt.show()
-
-# Treainig mode
-# cílem je aby data se kterými síť pracuje nebyly  od nikud alu uz data které zná
-# a také určit jak špatně to je určené
-
-
-
-
-
 """
 Co model LinearRegressionModel dělá?
 1. Ze začátku dostane náhodná čísla.
