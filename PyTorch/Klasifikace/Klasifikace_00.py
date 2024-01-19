003e3431be503a6e066c333b8bde75a3222475b0
Marek Bajer <marekbajer07@sezna.cz>
2024-01-19 17:39:01 +0100

    Rozdělení dat

--- PyTorch/Klasifikace/Klasifikace_00.py
+++ PyTorch/Klasifikace/Klasifikace_00.py
@@ -10,7 +10,13 @@ import pandas as pd
 # Data
 import sklearn
 from sklearn.datasets import make_circles
-
+from sklearn.model_selection import train_test_split
+# oddělí text caramy
+def print2(*args):
+    print("=======================================")
+    for arg in args:
+        print(arg)
+    print("=======================================")
 # vytvoření kol 1000 (kružnic)
 n_samples = 1000
 X, y = make_circles(n_samples,
@@ -31,8 +37,29 @@ plt.scatter(x=X[:, 0],
             y=X[:, 1],
             c=y,
             cmap=plt.cm.RdYlBu)
-plt.show()
+#plt.show()
 
+# 1.1 Převod dat do tensoru
+print(f"Tvar X: {X.shape} | Tvar y: {y.shape}")
+
+# Nahled na příklad vzorku vlastností a jmen
+X_sampel = X[0]
+y_sampel = y[0]
+print(f"Hodnota pro X : {X_sampel} to stejné pro y: {y_sampel}")
+print(f"Tvar X_sampel: {X_sampel.shape} | Tvar y_sampel: {y_sampel.shape}")
+
+# 1.2 převod + rodělení dat na treniková a na testová
+X = torch.from_numpy(X).type(torch.float) # převede s numpy array do torch tensoru
+y = torch.from_numpy(y).type(torch.float) # to stejné pro y
+print2(X[:5], y[:5])
+
+# Dělení dat
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,   # 0,2 -> 20% jako testova data
+                                                    random_state=42)
+
+print2(len(X_train), len(X_test), len(y_train), len(y_test))
 
 
 
